f1e52160a460078e705bebc6e36f6f18c38b5fe5
sinchang <sinchang@foxmail.com>
2017-05-04 12:51:11 +0800

    init

---
+++ unu.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const ncp = require("copy-paste")
+const request = require('request')
+const cliSpinners = require('cli-spinners')
+const logUpdate = require('log-update')
+const error = chalk.bold.red
+
+const questions = [{
+    type: 'input',
+    name: 'url',
+    message: 'What\'s the url you want to shorten?',
+    validate: function (value) {
+      const expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
+      const regex = new RegExp(expression)
+      const pass = value.match(regex)
+      if (pass) {
+        return true
+      }
+      return 'Please enter url'
+    }
+  },
+  {
+    type: 'input',
+    name: 'keyword',
+    message: 'What\'s the custom keyword you want? (optional)'
+  }
+]
+
+
+inquirer.prompt(questions).then(function (answers) {
+  let api = `https://u.nu/api.php?action=shorturl&format=json&url=${answers.url}&keyword=${answers.keyword || ''}`
+
+  const spinner = cliSpinners['dots']
+  let i = 0
+
+  const timer = setInterval(() => {
+    const frames = spinner.frames
+    logUpdate(frames[i = ++i % frames.length])
+  }, spinner.interval)
+
+  request(api, (error, response, body) => {
+    clearInterval(timer)
+    if (!error && response.statusCode == 200) {
+      const res = JSON.parse(body);
+      ncp.copy(res.shorturl)
+      return logUpdate(chalk.green(res.shorturl) + '  copied successfully ✔️')
+    }
+    return logUpdate(error('Something error, Please try again 😞'))
+  })
+})
